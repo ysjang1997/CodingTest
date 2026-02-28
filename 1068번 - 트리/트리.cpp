c61085b257efa812919b3a69e72d8e62122536e6
ysjang1997 <ysjang19978@gmail.com>
2026-03-01 00:19:05 +0900

    0301 // 2문제
    2636 - 치즈 (맵 + Connected Component + Dfs | 복기 참고)
    1068 - 트리 (vector[] 인접리스트 + Dfs | 복기 참고)

---
+++ 1068번 - 트리/트리.cpp
@@ -0,0 +1,85 @@
+﻿/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 1068                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: simba123 <boj.kr/u/simba123>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/1068                           #+#        #+#      #+#    */
+/*   Solved: 2026/02/28 23:26:13 by simba123      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+    n 입력
+
+    for n
+    -1 = root 저장
+
+    vecotr[부모] 에 자식을 추가
+
+    root부터 dfs
+
+    복기
+     - 트리는 좀더 많이 해봐야 할듯 해결방법이 생각이 안나서 해설 보고함
+
+     - 이 문제는 입력에서 이미 정렬된 값이 들어와서 for를 돌면서 바로바로 노드 삽입이 가능했지만
+       정렬되지 않은 입력이 들어오는 상황에 대한 생각을 해봐야할듯?
+     
+     - 처음에는 Node 구조체를 만들어서 할까 했는데 vector<int> 자료형을 가지는 배열을 만들어서
+       인접 리스트를 만들면 부모 노드와 연결된 자식을 저장하여 트리구조를 만들 수 있음.
+
+*/
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, root, del;
+vector<int> v[50];
+int num = 0;
+
+int Dfs(int node)
+{
+    int childcnt = 0;
+    int leafcnt = 0;
+    for (int &child : v[node])
+    {
+        if (child == del)
+            continue;
+
+        leafcnt += Dfs(child);
+        childcnt++;
+    }
+    if (childcnt == 0)
+        return 1;
+
+    return leafcnt;
+}
+
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> num;
+        if (num == -1)
+        {
+            root = i;
+            continue;
+        }
+        v[num].push_back(i);
+    }
+
+    cin >> num;
+    if (num == root)
+    {
+        cout << 0;
+        return 0;
+    }
+
+    del = num;
+    cout << Dfs(root);
+
+    return 0;
+}
---
+++ 2636번 - 치즈/치즈.cpp
@@ -0,0 +1,142 @@
+﻿/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2636                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: simba123 <boj.kr/u/simba123>                +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2636                           #+#        #+#      #+#    */
+/*   Solved: 2026/02/28 00:12:28 by simba123      ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+    1. 입력 받으면서 외곽을 바깥공기 세팅 (-1)
+
+    2. Air순회 - 바깥 공기와 닿아있다면 -1 (Dfs) 
+
+    3. arr순회 - cheese 카운팅, 녹게 될 치즈 melts에 추가
+
+    4. 치즈가 남았는지?
+        -> 없으면 turn 과 cheesecnt 출력
+        -> 있으면 반복 turn++, cheesecnt 캐싱, melts순회로 치즈 녹이기 이후 2번으로
+
+    복기
+    - Dfs의 주체가 될 객체에 대해서 고민해보기 (꼭 0인 객체를 기준으로 돌았어여 하는가)
+        -> 0부터 돌면 -1을 찾고 나를 바뀌야함
+        -> 1부터 돌면 0을찾고 0을 -1로 바꾸고 계속 진행가능
+    - visited는 탐색을 돌면서 이미 탐색한 객체를 체크하기 위해서 필요함
+    - for문 작성시 오타좀 내지마 제발
+*/
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int h, w;
+int arr[100][100] = {};
+int visited[100][100] = {};
+
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+
+int cheeseresult = 0;
+int turnresult = 0;
+
+void AirDfs(int y, int x)
+{
+    visited[y][x] = 1;
+    arr[y][x] = -1;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || ny >= h || nx < 0 || nx >= w)
+            continue;
+
+        if (visited[ny][nx] == 1)
+            continue;
+
+        if (arr[ny][nx] == 0)
+            AirDfs(ny, nx);
+    }
+}
+
+bool isMelt(int y, int x)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+
+        if (ny < 0 || ny >= h || nx < 0 || nx >= w)
+            continue;
+
+        if (arr[ny][nx] == -1)
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    cin >> h >> w;
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            cin >> arr[i][j];
+            if (i == 0 || i == h - 1 || j == 0 || j == w - 1)
+                arr[i][j] = -1;
+        }
+    }
+
+    while (true)
+    {
+        memset(visited, 0, sizeof(visited));
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if (arr[i][j] == -1 && visited[i][j] == 0)
+                    AirDfs(i, j);
+            }
+        }
+
+        vector<pair<int, int>> melts;
+        int _cheesecnt = 0;
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
+                if (arr[i][j] == 1)
+                {
+                    if (isMelt(i, j))
+                        melts.push_back({i, j});
+
+                    _cheesecnt++;
+                }
+            }
+        }
+
+        if (_cheesecnt == 0)
+            break;
+
+        cheeseresult = _cheesecnt;
+        turnresult++;
+
+        for (auto &melt : melts)
+        {
+            arr[melt.first][melt.second] = -1;
+        }
+    }
+
+    cout << turnresult << endl;
+    cout << cheeseresult;
+
+    return 0;
+}
